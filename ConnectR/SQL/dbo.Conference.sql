f6bc561941bb84ce41e60af2df72142c1133ff96
Derkse14 <daniel.dderksen@gmail.com>
2016-02-01 17:54:15 -0600

    Project start

---
+++ ConnectR/SQL/dbo.Conference.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Conference]
+(
+	[ConferenceId] INT NOT NULL PRIMARY KEY, 
+    [ProfileId] INT NOT NULL, 
+    [Content] NVARCHAR(MAX) NULL, 
+    [Image] BINARY(2048) NULL, 
+    CONSTRAINT [FK_Conference_Profile] FOREIGN KEY (ProfileId) REFERENCES Profile(ProfileId)
+)
