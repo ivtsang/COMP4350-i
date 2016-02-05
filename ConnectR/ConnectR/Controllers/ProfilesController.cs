65b9c2bd1273b3c817752c318d35061ec81e88fb
Lukas Lion <guitarshredder23@hotmail.com>
2016-02-05 15:10:06 -0600

    LL commit for profile functions.

---
+++ ConnectR/ConnectR/Controllers/ProfilesController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ConnectR.Models;
+using System.Web.Routing;
+using Microsoft.AspNet.Identity;
+
+namespace ConnectR.Controllers
+{
+    public class ProfilesController : Controller
+    {
+        private Entities db = new Entities();
+
+        protected override void Initialize(RequestContext requestContext)
+        {
+            if (requestContext.RouteData.GetRequiredString("action").Equals("create", StringComparison.InvariantCultureIgnoreCase))
+            {
+                int profileID = db.Profiles.Max(p => p.ProfileId);
+                ViewBag.ProfileID = profileID + 1;
+            }
+            base.Initialize(requestContext);
+        }
+
+        // GET: Profiles
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Profiles.ToListAsync());
+        }
+
+        // GET: Profiles/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profile profile = await db.Profiles.FindAsync(id);
+            if (profile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profile);
+        }
+
+        // GET: Profiles/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Profiles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ProfileId,UserId,FirstName,LastName,Age,Country,City,School,Degree,Image")] Profile profile)
+        {
+            if (ModelState.IsValid)
+            {
+                profile.UserId = User.Identity.GetUserId();
+                db.Profiles.Add(profile);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(profile);
+        }
+
+        // GET: Profiles/Edit/5
+        [Authorize]
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profile profile = await db.Profiles.FindAsync(id);
+            if (profile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profile);
+        }
+
+        // POST: Profiles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<ActionResult> Edit([Bind(Include = "ProfileId,UserId,FirstName,LastName,Age,Country,City,School,Degree,Image")] Profile profile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(profile).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(profile);
+        }
+
+        // GET: Profiles/Delete/5
+        [Authorize]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profile profile = await db.Profiles.FindAsync(id);
+            if (profile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profile);
+        }
+
+        // POST: Profiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Profile profile = await db.Profiles.FindAsync(id);
+            db.Profiles.Remove(profile);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
