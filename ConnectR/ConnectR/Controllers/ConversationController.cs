b34f4fc734259e40d6ac899e8e59c204b4901c75
Ivan Tsang <ivannonsense@hotmail.com>
2016-02-05 12:58:02 -0600

    forgot new files

---
+++ ConnectR/ConnectR/Controllers/ConversationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using ConnectR.Models;
+using System.Data.Entity;
+using System.Linq;
+
+namespace ConnectR.Controllers
+{
+    public class ConversationController : Controller
+    {
+        private ConnectRContext db = new ConnectRContext();
+
+        public ActionResult Index()
+        {
+            var Conversation = db.Conversations;
+            return View(Conversation.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            int userId = 1;
+            Message newMsg = new Message();
+            newMsg.user_id = userId;
+            return View();
+        }
+    }
+}
---
+++ ConnectR/ConnectR/Data_Initializer/TestDbInit.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ConnectR.Models;
+
+namespace ConnectR.DataInit
+{
+    //copy of this in tests as well, will remove once iter2
+    public class TestDbInit : DropCreateDatabaseAlways<ConnectRContext>
+    {
+        protected override void Seed(ConnectRContext context)
+        {
+            //users
+            context.Users.Add(new User() { user_id = 1 });
+            //context.Users.Add(new User() { user_id = 2 });
+            //context.Users.Add(new User() { user_id = 3 });
+
+            //profiles
+            context.Profiles.Add(new Profile() { ProfileId = 1, UserId = "1", FirstName = "bob" });
+
+            //conferences
+            context.Conferences.Add(new Conference() { ConferenceId = 1, ProfileId = 1, Title = "ye" });
+
+            //conversations
+            HashSet<User> users = new HashSet<User>();
+            User user1 = new User() { user_id = 2 };
+            User user2 = new User() { user_id = 3 };
+            users.Add(user1);
+            users.Add(user2);
+            Conversation conv = new Conversation() { conv_id = 11231, Users = users };
+            context.Conversations.Add(conv);
+
+            //messages
+            context.Messages.Add(new Message() { msg_id = 1, conv_id = 11231, user_id = 2, msg_date = DateTime.Now, msg_txt = "hello world", Conversation = conv });
+            context.Messages.Add(new Message() { msg_id = 2, conv_id = 11231, user_id = 2, msg_date = DateTime.Now, msg_txt = "greetings", Conversation = conv });
+            context.Messages.Add(new Message() { msg_id = 3, conv_id = 11231, user_id = 2, msg_date = DateTime.Now, msg_txt = "well met", Conversation = conv });
+            context.Messages.Add(new Message() { msg_id = 4, conv_id = 11231, user_id = 2, msg_date = DateTime.Now, msg_txt = "i am talking to myself", Conversation = conv });
+        }
+    }
+}
